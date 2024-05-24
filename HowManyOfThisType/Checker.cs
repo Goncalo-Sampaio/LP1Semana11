7ec9e5486d8400b9e236920bf6283cef7e3ebcf0
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-05-24 16:29:27 +0100

    Added xml documentation and comments

--- HowManyOfThisType/Checker.cs
+++ HowManyOfThisType/Checker.cs
@@ -5,8 +5,13 @@ using System.Threading.Tasks;
 
 namespace HowManyOfThisType
 {
+    /// <summary>
+    /// Class used to check if the items are of a certain type
+    /// </summary>
     public static class Checker
     {
+        //Method that takes an enumerable and checks each element to see
+        //if the element is of the provided type
          public static int HowManyOfType<T>(IEnumerable<object> items){
             int numberOfItems = 0;
             foreach(object item in items){
